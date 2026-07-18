86d1c8924389bc697d89ec4251cc67e79bdbbf84
MANOJ230527 <manojr2305@gmail.com>
2026-07-19 00:48:21 +0530

    Updated project files

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,20 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE = '';  // uses CRA proxy
+const BASE = process.env.REACT_APP_API_URL;
 
 export const registerUser = async (name, email, password) => {
-  const { data } = await axios.post('/auth/register', { name, email, password });
+  const { data } = await axios.post(`${BASE}/auth/register`, {
+    name,
+    email,
+    password,
+  });
   return data;
 };
 
 export const loginUser = async (email, password) => {
-  const { data } = await axios.post('/auth/login', { email, password });
+  const { data } = await axios.post(`${BASE}/auth/login`, {
+    email,
+    password,
+  });
   return data;
 };
 
 export const getMe = async (token) => {
-  const { data } = await axios.get('/auth/me', {
-    headers: { Authorization: `Bearer ${token}` }
+  const { data } = await axios.get(`${BASE}/auth/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
   return data;
 };
